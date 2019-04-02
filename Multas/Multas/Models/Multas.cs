4d04e402ffbb599b04831207a8686b8ba991b2e4
Pedro Vinha <pa.v.vinha@gmail.com>
2019-04-02 17:01:38 +0100

    Alteração inicial da base de dados

---
+++ Multas/Multas/Models/Multas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class Multas {
+
+        public int Id { get; set; }
+
+        public DateTime DataMulta { get; set; }
+
+        public decimal ValorMulta { get; set; }
+
+        public String Descricao { get; set; }
+
+
+    }
+}
